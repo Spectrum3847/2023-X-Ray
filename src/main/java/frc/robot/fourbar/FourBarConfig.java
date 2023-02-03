d7c6f8420771c30043e7b6deb87eebf742b5296c
rebeccabibye <76578313+rebeccabibye@users.noreply.github.com>
2023-02-02 18:56:54 -0600

    fourbar positions

--- src/main/java/frc/robot/fourbar/FourBarConfig.java
+++ src/main/java/frc/robot/fourbar/FourBarConfig.java
@@ -10,12 +10,12 @@ public class FourBarConfig extends AngleMechConfig {
     public final int maxAngle = 75000;
     public final int midAngle = 37000;
 
-    public final double cubeIntake = 50000;
+    public final double cubeIntake = 58000;
     public final double cubeMid = 60000;
     public final double cubeTop = 70000;
 
-    public final double coneIntake = 57389+1484;
-    public final double coneStandingIntake = 32378; 
+    public final double coneIntake = 59000;
+    public final double coneStandingIntake = 32378; //26197
     public final double coneShelf = 70000;
 
     public final double coneMid = 30000;
