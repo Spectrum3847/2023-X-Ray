2c8461202cb62d4d80c8b186a47fa35c22bdc935
Spectrum3847 <spectrum3847@gmail.com>
2023-02-17 16:15:38 -0600

    Updated to fix On-the-Fly Positions for Red Alliance side

--- src/main/java/frc/robot/trajectories/commands/PositionPaths.java
+++ src/main/java/frc/robot/trajectories/commands/PositionPaths.java
@@ -1,41 +1,77 @@
 package frc.robot.trajectories.commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.trajectories.TrajectoriesConfig;
 
 public class PositionPaths {
+    static double yPos;
+
     public static FollowOnTheFlyPath grid1Left() {
-        return new GeneratePathForScoring().GeneratePathScoring(TrajectoriesConfig.coneTTYPosition);
+        yPos = TrajectoriesConfig.coneTTYPosition;
+        if (DriverStation.getAlliance().equals(DriverStation.Alliance.Red)) {
+            yPos = TrajectoriesConfig.coneBBYPosition;
+        }
+        return new GeneratePathForScoring().GeneratePathScoring(yPos);
     }
 
     public static FollowOnTheFlyPath grid1Middle() {
-        return new GeneratePathForScoring().GeneratePathScoring(TrajectoriesConfig.cubeTYPosition);
+        yPos = TrajectoriesConfig.cubeTYPosition;
+        if (DriverStation.getAlliance().equals(DriverStation.Alliance.Red)) {
+            yPos = TrajectoriesConfig.cubeBYPosition;
+        }
+        return new GeneratePathForScoring().GeneratePathScoring(yPos);
     }
 
     public static FollowOnTheFlyPath grid1Right() {
-        return new GeneratePathForScoring().GeneratePathScoring(TrajectoriesConfig.coneTBYPosition);
+        yPos = TrajectoriesConfig.coneTBYPosition;
+        if (DriverStation.getAlliance().equals(DriverStation.Alliance.Red)) {
+            yPos = TrajectoriesConfig.coneBTYPosition;
+        }
+        return new GeneratePathForScoring().GeneratePathScoring(yPos);
     }
 
     public static FollowOnTheFlyPath grid2Left() {
-        return new GeneratePathForScoring().GeneratePathScoring(TrajectoriesConfig.coneMTYPosition);
+        yPos = TrajectoriesConfig.coneMTYPosition;
+        if (DriverStation.getAlliance().equals(DriverStation.Alliance.Red)) {
+            yPos = TrajectoriesConfig.coneMBYPosition;
+        }
+        return new GeneratePathForScoring().GeneratePathScoring(yPos);
     }
 
     public static FollowOnTheFlyPath grid2Middle() {
-        return new GeneratePathForScoring().GeneratePathScoring(TrajectoriesConfig.cubeMYPosition);
+        yPos = TrajectoriesConfig.cubeMYPosition;
+        return new GeneratePathForScoring().GeneratePathScoring(yPos);
     }
 
     public static FollowOnTheFlyPath grid2Right() {
-        return new GeneratePathForScoring().GeneratePathScoring(TrajectoriesConfig.coneMBYPosition);
+        yPos = TrajectoriesConfig.coneMBYPosition;
+        if (DriverStation.getAlliance().equals(DriverStation.Alliance.Red)) {
+            yPos = TrajectoriesConfig.coneMTYPosition;
+        }
+        return new GeneratePathForScoring().GeneratePathScoring(yPos);
     }
 
     public static FollowOnTheFlyPath grid3Left() {
-        return new GeneratePathForScoring().GeneratePathScoring(TrajectoriesConfig.coneBTYPosition);
+        yPos = TrajectoriesConfig.coneBTYPosition;
+        if (DriverStation.getAlliance().equals(DriverStation.Alliance.Red)) {
+            yPos = TrajectoriesConfig.coneTBYPosition;
+        }
+        return new GeneratePathForScoring().GeneratePathScoring(yPos);
     }
 
     public static FollowOnTheFlyPath grid3Middle() {
-        return new GeneratePathForScoring().GeneratePathScoring(TrajectoriesConfig.cubeBYPosition);
+        yPos = TrajectoriesConfig.cubeBYPosition;
+        if (DriverStation.getAlliance().equals(DriverStation.Alliance.Red)) {
+            yPos = TrajectoriesConfig.cubeTYPosition;
+        }
+        return new GeneratePathForScoring().GeneratePathScoring(yPos);
     }
 
     public static FollowOnTheFlyPath grid3Right() {
-        return new GeneratePathForScoring().GeneratePathScoring(TrajectoriesConfig.coneBBYPosition);
+        yPos = TrajectoriesConfig.coneBBYPosition;
+        if (DriverStation.getAlliance().equals(DriverStation.Alliance.Red)) {
+            yPos = TrajectoriesConfig.coneTTYPosition;
+        }
+        return new GeneratePathForScoring().GeneratePathScoring(yPos);
     }
 }
