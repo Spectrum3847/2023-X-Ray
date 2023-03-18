aa19f4a01e3ebc36ba39afffd53909b534aa202a
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2023-03-18 16:30:32 -0500

    updates

--- src/main/java/frc/robot/swerve/configs/XRAY2023.java
+++ src/main/java/frc/robot/swerve/configs/XRAY2023.java
@@ -49,7 +49,7 @@ public class XRAY2023 {
             ((6380 / 60) / driveGearRatio) * wheelDiameter * Math.PI * 1.33; // meters per //
     // second
     public static final double maxAutoVelocity =
-            ((6380 / 60) / driveGearRatio) * wheelDiameter * Math.PI * 1.1; // meters per //
+            ((6380 / 60) / driveGearRatio) * wheelDiameter * Math.PI * 0.9; // meters per //
     // second
     static final double maxAccel = maxVelocity * 1.5; // take 1/2 sec to get to max speed.
     static final double maxAngularVelocity =
