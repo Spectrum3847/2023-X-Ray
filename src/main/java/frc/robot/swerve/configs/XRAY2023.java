3ee3041a5ecb08aad95811427d57703d1d52a3d7
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2023-03-20 15:11:42 -0500

    updated swerve config

--- src/main/java/frc/robot/swerve/configs/XRAY2023.java
+++ src/main/java/frc/robot/swerve/configs/XRAY2023.java
@@ -12,10 +12,10 @@ import frc.robot.swerve.configTemplates.TuningConfig;
 
 public class XRAY2023 {
     /* Angle Offsets */
-    public static final double mod0angleOffset = 125.4199;
-    public static final double mod1angleOffset = 271.119;
-    public static final double mod2angleOffset = 61.75;
-    public static final double mod3angleOffset = 225.44531;
+    public static final double mod0angleOffset = 125.05455; // 125.4199;
+    public static final double mod1angleOffset = 271.11901; // 271.119;
+    public static final double mod2angleOffset = 61.02005; // 61.75;
+    public static final double mod3angleOffset = 224.98858; // 225.44531;
 
     /* CanBus Name */
     public static final String canBus = "3847";
