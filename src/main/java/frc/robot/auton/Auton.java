6b20d2cd62593a293f80b5a8c5f9292f67c4612b
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2023-03-21 16:03:43 -0500

    auton changes

--- src/main/java/frc/robot/auton/Auton.java
+++ src/main/java/frc/robot/auton/Auton.java
@@ -133,7 +133,9 @@ public class Auton {
                                 PathPlanner.loadPathGroup(
                                         "3 Ball Bottom w Balance",
                                         new PathConstraints(
-                                                AutonConfig.kMaxSpeed, AutonConfig.kMaxAccel))));
+                                                AutonConfig.kMaxSpeed, AutonConfig.kMaxAccel)))
+                        .andThen(new WaitCommand(0.1))
+                        .andThen(new BehindBalanceCommand()));
         autonChooser.addOption(
                 "4 Ball Bottom w Balance",
                 getAutoBuilder()
