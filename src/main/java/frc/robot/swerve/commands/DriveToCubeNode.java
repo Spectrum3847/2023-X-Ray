826122e4a0376650315776245c7cefe9eb9c27c5
Jonathan <jdaniel24@mail.strakejesuit.org>
2023-04-15 13:29:05 -0500

    build changes

--- src/main/java/frc/robot/swerve/commands/DriveToCubeNode.java
+++ src/main/java/frc/robot/swerve/commands/DriveToCubeNode.java
@@ -11,26 +11,26 @@ import edu.wpi.first.wpilibj2.command.PIDCommand;
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class DriveToCubeNode extends PIDCommand {
-  /** Creates a new DriveToCubeNode. */
-  public DriveToCubeNode() {
-    super(
-        // The controller that the command will use
-        new PIDController(0, 0, 0),
-        // This should return the measurement
-        () -> 0,
-        // This should return the setpoint (can also be a constant)
-        () -> 0,
-        // This uses the output
-        output -> {
-          // Use the output here
-        });
-    // Use addRequirements() here to declare subsystem dependencies.
-    // Configure additional PID options by calling `getController` here.
-  }
+    /** Creates a new DriveToCubeNode. */
+    public DriveToCubeNode() {
+        super(
+                // The controller that the command will use
+                new PIDController(0, 0, 0),
+                // This should return the measurement
+                () -> 0,
+                // This should return the setpoint (can also be a constant)
+                () -> 0,
+                // This uses the output
+                output -> {
+                    // Use the output here
+                });
+        // Use addRequirements() here to declare subsystem dependencies.
+        // Configure additional PID options by calling `getController` here.
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
