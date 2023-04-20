1981bfe833593707b194d2b5a0f07c5bb49218a6
Spectrum3847 <spectrum3847@gmail.com>
2023-04-20 17:02:54 -0500

    changes after qual 50

--- src/main/java/frc/robot/intakeLauncher/commands/HoldCone.java
+++ src/main/java/frc/robot/intakeLauncher/commands/HoldCone.java
@@ -7,6 +7,7 @@ package frc.robot.intakeLauncher.commands;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
+import frc.robot.elevator.Elevator;
 
 public class HoldCone extends CommandBase {
     double frontPos = 0;
@@ -29,21 +30,30 @@ public class HoldCone extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        double time =
-                Timer.getFPGATimestamp(); // If frontPos has moved 200 ticks then run intake full
-        // speed and 1/4 sec to timer
-        if (Robot.intake.frontMotor.getPosition() < frontPos - 200) {
-            Robot.intake.setPercentOutputs(0.3, 0.2, 0.0);
-            timer = time + 0.25;
-            // While timer is still within 1/4 sec of starting keep running intake
-        } else if (time - timer < 0) {
-            Robot.intake.setPercentOutputs(0.3, 0.2, 0.0);
-            // Once 1/4 sec is up stop the motor and reset frontPos to current one for a 1/4sec
-        } else if (time - timer < 0.25) {
-            Robot.intake.stopAll();
-            frontPos = Robot.intake.frontMotor.getPosition();
-            // After all that just wait for cone to slip again
+        if (Robot.elevator.getPosition()
+                >= Elevator.inchesToFalcon(Elevator.config.holdConeHeight)) {
+            // this should be fine in the if statement instead of out since all logic will only run
+            // in this if statement
+            double time =
+                    Timer.getFPGATimestamp(); // If frontPos has moved 200 ticks then run intake
+            // full
+            // speed and 1/4 sec to timer
+            if (Robot.intake.frontMotor.getPosition() < frontPos - 200) {
+                Robot.intake.setPercentOutputs(0.3, 0.2, 0.0);
+                timer = time + 0.25;
+                // While timer is still within 1/4 sec of starting keep running intake
+            } else if (time - timer < 0) {
+                Robot.intake.setPercentOutputs(0.3, 0.2, 0.0);
+                // Once 1/4 sec is up stop the motor and reset frontPos to current one for a 1/4sec
+            } else if (time - timer < 0.25) {
+                Robot.intake.stopAll();
+                frontPos = Robot.intake.frontMotor.getPosition();
+                // After all that just wait for cone to slip again
+            } else {
+                Robot.intake.stopAll();
+            }
         } else {
+            System.out.println("elevator position stopped hold cone from running");
             Robot.intake.stopAll();
         }
     }
